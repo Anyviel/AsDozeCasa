13f201f908ba4768e6e9fdb72e46fc10441361ec
Anyviel <danyvielcos@gmail.com>
2019-10-20 21:26:44 -0300

    Finalizações

--- main.js
+++ main.js
@@ -8,19 +8,19 @@ const inputElement = document.querySelector('#formulario input');
 const buttonElement = document.querySelector('#btn');
 
 buttonElement.addEventListener('click', valor => {
-    update(inputElement.value)
+    update(inputElement.value) //Executa a função Update
 })
 
 function update(valor) {
-    let { dozeCasas, cavaleiros } = window;
-    if (valor >= 1 && valor <= 12) {
+    let { dozeCasas, cavaleiros } = window; //Recebem os valores do arquivo Data.js
+    if (valor >= 1 && valor <= 12) { //Verificação dos valores de cada Casa.
         const cavaleiro = cavaleiros[parseInt(valor) - 1];
 
         dozeCasas.nome.innerText = cavaleiro.nome;
         dozeCasas.casa.innerText = cavaleiro.casa;
         dozeCasas.cover.style.backgroundImage = 'url(' + cavaleiro.cover + ')';
     } else {
-        alert('Digite um número de 1 e 12');
+        alert('Digite um número de 1 e 12'); //Retorna um Alerta caso não seja digitado nenhum dos valores entre 1 e 12
     }
 }
 
@@ -33,13 +33,13 @@ ulCircles.addEventListener("animationstart", event => {
     document.querySelector("body").style.overflow = "hidden";
 });
 
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 150; i++) {
     const li = document.createElement('li');
 
     //Constante para a variação dos valores.
     const random = (min, max) => Math.random() * (max - min) + min;
 
-    const size = Math.floor(random(10, 50)); //Cria os tamanhos variados dos círculos.
+    const size = Math.floor(random(3, 5)); //Cria os tamanhos variados dos círculos.
     const position = random(1, 95); //Modifica aleatoriamente as possições entre 1 e 95.
     const delay = random(5, 0.1); //Gera um atraso para a movimentação dos círculos.
     const duration = random(24, 12); //Define aleatoriamente o tempo de duração do efeito.
